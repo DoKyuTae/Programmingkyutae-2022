2fae1b27aa7d0a62f786fb9cb3b122295ef02af5
DoKyuTae <112994049+DoKyuTae@users.noreply.github.com>
2022-10-19 15:21:15 +0900

    6week prob 1

--- 6th-week/Prob_1/소스.cpp
+++ 6th-week/Prob_1/소스.cpp
@@ -1,28 +1,28 @@
 #include <stdio.h>
-#include <string.h>
+#include <string.h>                                        // strcmp 사용하기 위함
 
-#define PASSWORLD "dokyu19"
-#define SIZE 50
+#define PASSWORLD "dokyu19"                                // 비밀번호 설정
+#define SIZE 50                                            // 배열 크기 설정
 
 int main(void)
 {
-	char mypass[] = { PASSWORLD };
-	char inpass[SIZE] = { 0 };
+	char mypass[] = { PASSWORLD };                        // 비밀번호 변수 설정
+	char inpass[SIZE] = { 0 };                            // 입력받을 비밀번호 변수 설정
 
 	while (1)
 	{
 		printf("input password : ");
-		scanf_s("%s", inpass, SIZE);
+		scanf_s("%s", inpass, SIZE);                      // 비밀번호 입력, 배열 위치 설정
 
 		
 		
-			if (strcmp(inpass, mypass) == 0)
+			if (strcmp(inpass, mypass) == 0)             // 설정한 비밀번호와 입력된 비밀번호 비교
 			{
-				printf("nomal termination..\n");
+				printf("nomal termination..\n");         // 맞으면 멘트 출력 후 프로그램 종료
 				break;
 			}
 			
-			printf("not matched, retry..\n");
+			printf("not matched, retry..\n");            // 틀리면 멘트 출력 후 다시 입력 단계로 돌아감
 	}
 
 	return 0;
