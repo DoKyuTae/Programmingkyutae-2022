61b475568b44a4b7af092cab60196bb7741726e4
DoKyuTae <112994049+DoKyuTae@users.noreply.github.com>
2022-10-21 01:12:05 +0900

    6week prob 3

--- 6th-week/Prob_3/소스.cpp
+++ 6th-week/Prob_3/소스.cpp
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define MATRIX_A 3
+#define MATRIX_B 5
+#define MAX_NUM 100
+
 void GenRandSeed()
 {
 	srand((unsigned int)(time(NULL)));
@@ -9,39 +13,80 @@ void GenRandSeed()
 }
 
 
-
-unsigned int GenRandNum(unsigned int nRange)
-{
-	unsigned int nRes = 0;
-	unsigned int nNum = rand();
-	nRes = ((unsigned int)(nNum) % (nRange));
-	return nRes;
-}
-
-
-int main(void)
+int cal()
 {
 	int i = 0;
 	int j = 0;
 	int k = 0;
 	int p = 0;
-    int a[3][5], b[3][5];
-	GenRandSeed();
-	int nRange = 0;
-	
+	int res = 0;
+	int a[MATRIX_A][MATRIX_B];
+	int b[MATRIX_B][MATRIX_A];
+	int c[MATRIX_B][MATRIX_B];
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < MATRIX_A; i++)
 	{
-		for (j = 0; j < 5; j++)
+		for (k = 0; k < MATRIX_A; k++)
 		{
-			int nRes = GenRandNum(nRange);
-			a[i][j] = GenRandNum(nRange);
-			printf("matrix : %d", a[i][j]);
+			for (j = 0, p = 0; j < MATRIX_B; j++, k++)
+			{
+				res += a[i][j] * b[p][k];
+			}
+			
+			res = c[i][j];
+            printf("calculate matrix A and matrix B : \t%d", res);
+		
+		}
+
+		printf("\n");
+
+	}
+	
+	return res;
+}
+
+int main(void)
+{
+	int nRange = 0;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int p = 0;
+	int a[MATRIX_A][MATRIX_B];
+	int b[MATRIX_B][MATRIX_A];
+	int result;
+
+	result = cal();
+	
+	GenRandSeed();
+	
+	printf("matrix A : \n");
+	
+	for (i = 0; i < MATRIX_A; i++)
+	{
+		for (j = 0; j < MATRIX_B; j++)
+		{
+			a[i][j] = rand() * MAX_NUM / RAND_MAX;
+			printf("\t%d", a[i][j]);
 		}
 		j = 0;
-		printf(" ");
+		printf("\n");
 	}
-	printf(" ");
+	printf("");
+
+	printf("matrix B : \n");
+
+	for (p = 0; p < MATRIX_B; p++)
+	{
+		for (k = 0; k < MATRIX_A; k++)
+		{
+			b[p][k] = rand() * MAX_NUM / RAND_MAX;
+			printf("\t%d", b[p][k]);
+		}
+		k = 0;
+		printf("\n");
+	}
+	printf("");
 
 	return 0;
 }
