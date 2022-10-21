704870aa4a18f8fa87fb17aeb5ecc255a2306c0b
DoKyuTae <112994049+DoKyuTae@users.noreply.github.com>
2022-10-21 12:06:49 +0900

    6week prob 3

--- 6th-week/Prob_3/소스.cpp
+++ 6th-week/Prob_3/소스.cpp
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
+
 #define MATRIX_A 3
-#define MATRIX_B 5
+#define MATRIX_B 3
 #define MAX_NUM 100
+
 
 void GenRandSeed()
 {
@@ -12,37 +14,9 @@ void GenRandSeed()
 	return;
 }
 
-int cal()
-{
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	int p = 0;
-	int a[MATRIX_A][MATRIX_B] = { 0 };
-	int b[MATRIX_B][MATRIX_A] = { 0 };
-	int c[MATRIX_A][MATRIX_A] = { 0 };
-
-
-	for (i = 0; i < MATRIX_A; i++)
-	{
-		for (k = 0; k < MATRIX_A; k++)
-		{
-			for (j = 0; j < MATRIX_B; j++)
-			{
-				c[i][k] += (a[i][j] * b[j][k]);
-			}
-
-		}
-	}
-
-	return 0;
-}
-
-
 
 int main(void)
 {
-	int nRange = 0;
 	int i = 0;
 	int j = 0;
 	int k = 0;
@@ -50,8 +24,6 @@ int main(void)
 	int a[MATRIX_A][MATRIX_B] = { 0 };
 	int b[MATRIX_B][MATRIX_A] = { 0 };
 	int res = 0;
-
-	res = cal();
 
 	GenRandSeed();
 	
@@ -64,7 +36,6 @@ int main(void)
 			a[i][j] = rand() * MAX_NUM / RAND_MAX;
 			printf("\t%d", a[i][j]);
 		}
-		j = 0;
 		printf("\n");
 	}
 	printf("");
@@ -78,12 +49,26 @@ int main(void)
 			b[p][k] = rand() * MAX_NUM / RAND_MAX;
 			printf("\t%d", b[p][k]);
 		}
-		k = 0;
 		printf("\n");
 	}
 	printf("");
 
-	printf("calculate matrix A and matrix B : \t%d", res);
+	printf("multiplication of matrices A and B : \n");
+	
+	for (i = 0; i < MATRIX_A; i++)
+	{
+		for (k = 0; k < MATRIX_A; k++)
+		{
+			for (j = 0, p = 0; j < MATRIX_B; j++, p++)
+			{
+				res += a[i][j] * b[p][k];
+			}
+			printf("\t%d", res);
+		}
+
+		printf("\n");
+
+	}
 
 	return 0;
 }
