251798bd14fbc1d8e524a0512321826ae019a129
DoKyuTae <112994049+DoKyuTae@users.noreply.github.com>
2022-11-16 19:44:22 +0900

    9week prob 8

--- 9th-week/Prob_8/소스.cpp
+++ 9th-week/Prob_8/소스.cpp
@@ -4,10 +4,21 @@
 
 int main(void)
 {
-	char str[] = "C:\Document\Github\Test\test.txt";
-	char *res = str
+	char str[] = "C:\ Document\ Github\ Test\ test.txt";  // 띄어쓰기까지 문자열 32, NULL 까지 33
+	char* res;
+	res = strtok(str, " ");
+	int len = sizeof(str);
 
 
+	printf("%d\n", len);
+
+
+	while (res != NULL)
+	{
+		
+		printf("%s\n", res);
+		res = strtok(NULL, " ");
+	}
 
 	return 0;
 }
