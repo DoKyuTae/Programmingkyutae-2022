4ae88649b05df317b6d14902c344933193ab7723
DoKyuTae <112994049+DoKyuTae@users.noreply.github.com>
2022-11-22 18:44:50 +0900

    9week prob 1

--- 9th-week/Prob_1/소스.cpp
+++ 9th-week/Prob_1/소스.cpp
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_ZERO 0
-#define NUM_FIR 1
+#define NUM_ZERO 0                                           // 초기화 숫자
+#define NUM_FIR 1                                            // 연산에 필요한 숫자
 #define NUM_SEC 2
-#define NUM_STR 5
+#define NUM_STR 5                                            // 한 줄에 5개씩 출력
 
 int main(void)
 {
-	int *pi;
+	int *pi;                                                 // 포인터 설정, 변수 설정
 	int num;
 	int i, j = NUM_ZERO;
 
 	printf("input a positive number : ");
 	scanf_s("%d", &num);
 
-	pi = (int*)malloc((num - NUM_FIR) * sizeof(int));
+	pi = (int*)malloc((num - NUM_FIR) * sizeof(int));        // 저장공간 할당
 	
 	for (i = NUM_ZERO; i < num - NUM_FIR; i++)
 	{
@@ -24,7 +24,7 @@ int main(void)
 	
 	i = NUM_ZERO;
 
-	while (NUM_FIR)
+	while (NUM_FIR)                                          // 입력한 숫자 범위 안에서 소수를 찾는 과정
 	{
 		while ((pi[i] == NUM_ZERO) && (i < num - NUM_SEC))
 		{
@@ -44,7 +44,7 @@ int main(void)
 			i++;
 	}
 	
-	for (i = NUM_ZERO; i < num - NUM_SEC; i++)
+	for (i = NUM_ZERO; i < num - NUM_SEC; i++)               // 소수를 한줄에 5개씩 출력하고 소수가 아니면 X를 출력
 	{
 		if (pi[i] != NUM_ZERO)
 		{
