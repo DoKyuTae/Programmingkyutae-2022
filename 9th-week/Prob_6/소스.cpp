931a66c01c4c276431217436caa3ade56884ed58
DoKyuTae <112994049+DoKyuTae@users.noreply.github.com>
2022-11-22 19:04:37 +0900

    9week prob 6

--- 9th-week/Prob_6/소스.cpp
+++ 9th-week/Prob_6/소스.cpp
@@ -2,23 +2,23 @@
 
 #define DIR_NUM 4
 
-typedef enum { CYAN, MAGENTA, YELLOW = 5, BLACK } COLOR;
+typedef enum { CYAN, MAGENTA, YELLOW = 5, BLACK } COLOR;               // 구조체 선언 및 재정의
 typedef enum { UP, DOWN, LEFT, RIGHT } ARROW;
 
 int main(void)
 {
-	COLOR my_color = YELLOW;
+	COLOR my_color = YELLOW;                                           // 변수 설정
 	int c;
 	int direction = UP;
 
-	for (c = CYAN; c <= BLACK; c++)
+	for (c = CYAN; c <= BLACK; c++)                                    // 저장된 구조체 값에서 원하는 값 찾기
 	{
 		direction ++;
 		direction = direction % DIR_NUM;
 		if (c == my_color) break;
 	}
 
-	switch (direction)
+	switch (direction)                                                 // 방향에 따른 출력 값
 	{
 	case UP : 
 		printf("Current Direction : up");
