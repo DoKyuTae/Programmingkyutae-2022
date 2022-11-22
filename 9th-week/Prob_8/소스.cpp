65aa3f469184fda56e44eff71be812c0419530c3
DoKyuTae <112994049+DoKyuTae@users.noreply.github.com>
2022-11-22 19:27:34 +0900

    9week prob 8

--- 9th-week/Prob_8/소스.cpp
+++ 9th-week/Prob_8/소스.cpp
@@ -3,26 +3,26 @@
 #include <string.h>
 
 
-#define MAX_SIZE 1000
-#define STR_LOC 1
-#define STR_LOC_NAME 5
-#define STR_LOC_EXTE 6
+#define MAX_SIZE 100                                                    // 문자열의 최대 크기
+#define STR_LOC 1                                                       // 문자열 위치
+#define STR_LOC_NAME 5                                                  // 문자열에서 파일명 위치
+#define STR_LOC_EXTE 6                                                  // 문자열에서 확장자 위치
 
 
 int main(void)
 {
-    int i;
+    int i;                                                              // 변수 설정
     int count = 0;
    
-    char str[MAX_SIZE] = "C:\\Document\\Github\\Test1\\test.txt";
+    char str[MAX_SIZE] = "C:\\Document\\Github\\Test1\\test.txt";       // 문자열 선언
     char* pstr = str;
     
     char* cut_str[MAX_SIZE] = { NULL, };
     char* text;
     
-    char* extract = strtok_s(pstr, "\\", &text);
+    char* extract = strtok_s(pstr, "\\", &text);                        // \\ 기준으로 자른다는 의미
     
-    while (extract != NULL)
+    while (extract != NULL)                                             // 문자열에서 \\ 기준으로 하여 자르고 저장
     {
         cut_str[count] = extract;
         count++;
@@ -31,7 +31,7 @@ int main(void)
 
     extract = strtok_s(cut_str[count - STR_LOC], ".", &text);
     
-    while (extract != NULL)
+    while (extract != NULL)                                             // 문자열에서 " " 기준으로 자름
     {
         cut_str[count] = extract;
         count++;
@@ -39,6 +39,7 @@ int main(void)
     }
 
     printf("Top File Name : %s \nExtension : %s\n", cut_str[STR_LOC_NAME], cut_str[STR_LOC_EXTE]);
+                                                                        // 파일명과 확장자 부분 출력
 
     return 0;
 
