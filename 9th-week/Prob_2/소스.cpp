69100ff40f45418d8b663774fbefe9f79930436d
DoKyuTae <112994049+DoKyuTae@users.noreply.github.com>
2022-11-22 18:34:08 +0900

    9week prob 2

--- 9th-week/Prob_2/소스.cpp
+++ 9th-week/Prob_2/소스.cpp
@@ -3,10 +3,13 @@
 #include <string.h>
 
 
-#define MAX_STR 100
+#define MAX_STR 100                                     // 문자열의 크기
+#define prince_page 136                                 // 책 어린왕자 페이지 수
+#define match_page 32                                   // 책 성냥팔이소녀 페이지 수
+#define oldman_page 160                                 // 책 노인과 바다 페이지 수
 
 
-struct profile
+struct profile                                          // 구조체 설정
 {
 	char book[MAX_STR];
 	char author[MAX_STR];
@@ -14,14 +17,14 @@ struct profile
 	char price[MAX_STR];
 };
 
-void print_prince(void);
+void print_prince(void);                                // 책 정보 출력하는 함수 설정
 void print_match(void);
 void print_oldman(void);
 
 int main(void)
 {
 	
-	print_prince();
+	print_prince();                                     // 책 정보를 출력하는 함수 호출
 	print_match();
 	print_oldman();
 
@@ -29,13 +32,13 @@ int main(void)
 
 }
 
-void print_prince(void)
+void print_prince(void)                                 // 책 어린왕자 정보를 출력하는 함수 정의
 {
 	struct profile prince;
 
 	strcpy_s(prince.book, "The Little Prince");
 	strcpy_s(prince.author, "Antoine Marie Roger De Saint Exupery");
-	prince.page = 136;
+	prince.page = prince_page;
 	strcpy_s(prince.price, "12000 Won");
 
 	printf("Book Title : %s\n", prince.book);
@@ -45,13 +48,13 @@ void print_prince(void)
 }
 
 
-void print_match(void)
+void print_match(void)                                  // 책 성냥팔이소녀 정보를 출력하는 함수 정의
 {
 	struct profile match;
 
 	strcpy_s(match.book, "The Little Match Girl");
 	strcpy_s(match.author, "Hans Christian Andersen");
-	match.page = 32;
+	match.page = match_page;
 	strcpy_s(match.price, "9500 Won");
 
 	printf("Book Title : %s\n", match.book);
@@ -61,13 +64,13 @@ void print_match(void)
 }
 
 
-void print_oldman(void)
+void print_oldman(void)                                 // 책 노인과 바다 정보를 출력하는 함수 정의
 {
 	struct profile oldman;
 
 	strcpy_s(oldman.book, "The Old Man and the Sea");
 	strcpy_s(oldman.author, "Ernest Hemingway");
-	oldman.page = 160;
+	oldman.page = oldman_page;
 	strcpy_s(oldman.price, "8000 Won");
 	
 	printf("Book Title : %s\n", oldman.book);
