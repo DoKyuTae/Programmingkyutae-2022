af013c9b29ec99892d0663745ff0ba06a74b871d
DoKyuTae <112994049+DoKyuTae@users.noreply.github.com>
2022-11-22 17:47:22 +0900

    9week prob 8

--- 9th-week/Prob_8/소스.cpp
+++ 9th-week/Prob_8/소스.cpp
@@ -1,24 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+
+#define MAX_SIZE 1000
+#define STR_LOC 1
+#define STR_LOC_NAME 5
+#define STR_LOC_EXTE 6
 
 
 int main(void)
 {
-	char str[] = "C:\ Document\ Github\ Test\ test.txt";  // 띄어쓰기까지 문자열 32, NULL 까지 33
-	char* res;
-	res = strtok(str, " ");
-	int len = sizeof(str);
+    int i;
+    int count = 0;
+   
+    char str[MAX_SIZE] = "C:\\Document\\Github\\Test1\\test.txt";
+    char* pstr = str;
+    
+    char* cut_str[MAX_SIZE] = { NULL, };
+    char* text;
+    
+    char* extract = strtok_s(pstr, "\\", &text);
+    
+    while (extract != NULL)
+    {
+        cut_str[count] = extract;
+        count++;
+        extract = strtok_s(NULL, "\\", &text);
+    }
 
+    extract = strtok_s(cut_str[count - STR_LOC], ".", &text);
+    
+    while (extract != NULL)
+    {
+        cut_str[count] = extract;
+        count++;
+        extract = strtok_s(NULL, ".", &text);
+    }
 
-	printf("%d\n", len);
+    printf("Top File Name : %s \nExtension : %s\n", cut_str[STR_LOC_NAME], cut_str[STR_LOC_EXTE]);
 
+    return 0;
 
-	while (res != NULL)
-	{
-		
-		printf("%s\n", res);
-		res = strtok(NULL, " ");
-	}
-
-	return 0;
 }
